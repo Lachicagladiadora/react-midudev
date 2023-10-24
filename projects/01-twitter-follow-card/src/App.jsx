2ae3f8437a255e083909b304ff321b49cdfd7d6b
Lachicagladiadora <200379@unsaac.edu.pe>
2023-10-23 23:41:46 -0500

    wip: add follow card

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,28 +1,42 @@
 import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export const App = () => {
   return (
     <div>
       <h1>Twitter Follow Card</h1>
-      <article className="tw-follow-card">
-        <header className="tw-follow-card-header">
-          <img
-            className="tw-follow-card-image"
-            src="https://2.bp.blogspot.com/-0IVoz3YrCVo/T0p6gBdlUUI/AAAAAAAAQJc/O3SzL5GPsyU/s1600/era+bad+michael+jackson+(80).jpg"
-            alt="Michael Jackson avatar"
-          />
-          <div className="tw-follow-card-info">
-            <strong className="tw-follow-card-info-name">
-              Michael Joseph Jackson
-            </strong>
-            <span className="tw-follow-card-info-userName">
-              @michaeljackson
-            </span>
-          </div>
-        </header>
-        <aside>
-          <button className="tw-follow-card-button">Follow</button>
-        </aside>
+      <article>
+        <TwitterFollowCard
+          isFollowing={true}
+          userName={"michaeljackson"}
+          url={
+            "https://2.bp.blogspot.com/-0IVoz3YrCVo/T0p6gBdlUUI/AAAAAAAAQJc/O3SzL5GPsyU/s1600/era+bad+michael+jackson+(80).jpg"
+          }
+          name={"Michael Joseph Jackson"}
+        />
+
+        <TwitterFollowCard
+          isFollowing={false}
+          userName={"bonovox"}
+          url={
+            "https://i.pinimg.com/474x/b3/a4/82/b3a482f88645f77cde073ecdd363787d.jpg"
+          }
+          name={"Paul David Hewson"}
+        />
+        <TwitterFollowCard
+          isFollowing
+          userName={"steveperry"}
+          url={
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOJktDoI1-1hLxR7SV5rv0qUI87aIrTS4DPnj9zFYY5s_7iYc8FX8y3MtlUhZ_DyxVLME&usqp=CAU"
+          }
+          name={"Stephen Ray Perry"}
+        />
+        <TwitterFollowCard
+          isFollowing={false}
+          userName={"jonbonjovi"}
+          url={"https://iscale.iheart.com/catalog/artist/42212"}
+          name={"John Francis Bongiovi"}
+        />
       </article>
     </div>
   );
--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,1 +1,25 @@
-export const Twitter
+export const TwitterFollowCard = ({ url, userName, name, isFollowing }) => {
+  return (
+    <section className="tw-follow-card">
+      <header className="tw-follow-card-header">
+        <img
+          className="tw-follow-card-image"
+          src={url}
+          alt={`${name} avatar`}
+        />
+        <div className="tw-follow-card-info">
+          <strong className="tw-follow-card-info-name">{name}</strong>
+          <span className="tw-follow-card-info-userName">@{userName}</span>
+        </div>
+      </header>
+      <aside>
+        <button
+          className="tw-follow-card-button"
+          onClick={() => console.log(isFollowing)}
+        >
+          Follow
+        </button>
+      </aside>
+    </section>
+  );
+};
