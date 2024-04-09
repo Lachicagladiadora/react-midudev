14ea268713a9fdff044f5760d316742e1528b57b
Lachicagladiadora <200379@unsaac.edu.pe>
2024-04-08 23:06:47 -0500

    feat: add styles

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,8 +1,19 @@
+<<<<<<< Updated upstream
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
+=======
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+>>>>>>> Stashed changes
 
 export const App = () => {
   return (
+<<<<<<< Updated upstream
     <div>
       <h1>Twitter Follow Card</h1>
       <article>
@@ -41,3 +52,31 @@ export const App = () => {
     </div>
   );
 };
+=======
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
+>>>>>>> Stashed changes
