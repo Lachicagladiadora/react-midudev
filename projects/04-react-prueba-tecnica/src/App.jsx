b57e2d66ec449c363f23b625cfbfb50fcb07c0ec
Lachicagladiadora <200379@unsaac.edu.pe>
2024-05-06 22:44:44 -0500

    wip: 04 prueba tecnica

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+      <h1>Prueba tecnica</h1>
+      <section>
+        <h2>hecho aleatorio</h2>
+      </section>
+      <section>
+        <h2>first word del hecho aleatorio</h2>
+      </section>
+      <section>
+        <h2>gato con la first word del hecho aleatorio</h2>
+      </section>
+    </>
+  )
+}
