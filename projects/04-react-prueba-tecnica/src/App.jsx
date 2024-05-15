18f38873afe264b54fcf317ca8d8bb098ece2e9b
Lachicagladiadora <200379@unsaac.edu.pe>
2024-05-14 22:43:04 -0500

    refactor: complete useData hook

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,42 +1,12 @@
 import React from 'react'
-// import { getFact } from './utils'
 import { useData } from './useData'
 
 export const App = () => {
-  // const [randomFact, setRandomFact] = useState('')
-  // const [firstWord, setFirstWord] = useState('')
-  // const [urlCatImage, setUrlCatImage] = useState('')
 
-  // // fact
-  // const updateFact = async () => {
-  //   const factData = await getFact()
-  //   setRandomFact(factData)
-  // }
+  const { randomFact, urlCatImage } = useData()
 
-  // const getCatImage = async () => {
-  //   // word
-  //   const words = randomFact.split(' ')
-  //   const word = words[0].toLowerCase()
-  //   setFirstWord(word)
-  //   // image
-  //   const img = await fetch(`https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red`)
-  //   setUrlCatImage(img.url)
-  // }
-
-  // const getNewFact = () => {
-  //   updateFact()
-  // }
-
-  // useEffect(() => {
-  //   if (!randomFact) return
-  //   getCatImage()
-
-  // }, [randomFact])
-
-  // useEffect(() => {
-  //   updateFact()
-  // }, [])
-  const { randomFact, newFact, urlCatImage } = useData()
+  console.log({ randomFact })
+  console.log({ urlCatImage })
 
   return (
     <>
@@ -46,13 +16,10 @@ export const App = () => {
           <h2>Random fact</h2>
           <p>{randomFact}</p>
           <h2>First word</h2>
-          <p style={{ fontWeight: 'bold' }}>{firstWord}</p>
-          {/* <button onClick={getNewFact}>Other fact</button> */}
-          <p>{newFact}</p>
         </section>
         <section>
           <h2>Image</h2>
-          <img src={urlCatImage} alt={`image-${firstWord}`} height={500} />
+          <img src={urlCatImage} alt={`image-`} height={500} />
         </section>
       </div>
     </>
--- projects/04-react-prueba-tecnica/src/useData.jsx
+++ projects/04-react-prueba-tecnica/src/useData.jsx
@@ -3,10 +3,9 @@ import { getFact } from './utils'
 
 export const useData = () => {
   const [randomFact, setRandomFact] = useState('')
-  // const [firstWord, setFirstWord] = useState('')
   const [urlCatImage, setUrlCatImage] = useState('')
 
-  // // fact
+  // fact
   const updateFact = async () => {
     const factData = await getFact()
     setRandomFact(factData)
@@ -16,7 +15,6 @@ export const useData = () => {
     // word
     const words = randomFact.split(' ')
     const word = words[0].toLowerCase()
-    setFirstWord(word)
     // image
     const img = await fetch(`https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red`)
     setUrlCatImage(img.url)
@@ -28,32 +26,13 @@ export const useData = () => {
   }
 
   useEffect(() => {
+    updateFact()
+  }, [])
+
+  useEffect(() => {
     if (!randomFact) return
     getCatImage()
-
   }, [randomFact])
 
-  // useEffect(() => {
-  //   updateFact()
-  // }, [])
-
-  // return (
-  //   <>
-  //     <h1>Technical test: Cat's app</h1>
-  //     <div style={{ display: 'flex', gap: '40px' }}>
-  //       <section>
-  //         <h2>Random fact</h2>
-  //         <p>{randomFact}</p>
-  //         <h2>First word</h2>
-  //         <p style={{ fontWeight: 'bold' }}>{firstWord}</p>
-  //         <button onClick={getNewFact}>Other fact</button>
-  //       </section>
-  //       <section>
-  //         <h2>Image</h2>
-  //         <img src={urlCatImage} alt={`image-${firstWord}`} height={500} />
-  //       </section>
-  //     </div>
-  //   </>
-  // )
-  return { randomFact, getNewFact, urlCatImage }
+  return { randomFact, urlCatImage }
 }
