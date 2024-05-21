170675fe1b6e0743f38d81960abe442d41359fd1
Lachicagladiadora <200379@unsaac.edu.pe>
2024-05-20 23:12:19 -0500

    wip: home template

--- projects/05-react-search-movies/src/App.jsx
+++ projects/05-react-search-movies/src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      hello
-    </>
+    <div>
+      <header>
+        <h1>Movies searcher</h1>
+        <form >
+          <input type="text" placeholder='The farewell concubine, Your name engraved' />
+          <button>Search</button>
+        </form>
+      </header>
+    </div>
   )
 }
 
