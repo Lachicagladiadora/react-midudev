3eb552f8216c1456c9e75615b4c18bc226efe500
Lachicagladiadora <200379@unsaac.edu.pe>
2024-05-21 23:29:20 -0500

    wip: update template and mocks

--- projects/05-react-search-movies/src/App.jsx
+++ projects/05-react-search-movies/src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css'
+import { RESPONSE_NOT_FOUND } from './mocks/notFound'
+import { RESPONSE } from './mocks/response'
 
 function App() {
+  const [response, setResponse] = useState()
+
+  const existResponse = () => {
+    if (response.length() > 0) {
+      setResponse(RESPONSE.Search)
+      return true
+    } else {
+      setResponse(RESPONSE_NOT_FOUND)
+      return false
+    }
+  }
 
   return (
     <div>
@@ -11,6 +24,23 @@ function App() {
           <button>Search</button>
         </form>
       </header>
+      <main>
+        {existResponse() ? (
+          response.map((cur) => (
+            <div key={cur.imdbID}>
+              <h1>{cur.Title}</h1>
+              <img src={cur.Poster} alt={`poster from ${cur.Title}`} />
+              <div>
+                <p>{cur.Type}</p>
+                <p>{cur.Year}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>{response.Error}</p>
+        )}
+
+      </main>
     </div>
   )
 }
---
+++ projects/05-react-search-movies/src/mocks/notFound.tsx
@@ -0,0 +1,4 @@
+export const RESPONSE_NOT_FOUND = {
+  Response: "False",
+  Error: "Movie not found!",
+};
