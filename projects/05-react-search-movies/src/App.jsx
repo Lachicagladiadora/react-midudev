baaf928f970a0f6235c3b25a78dd35095fd6e278
Lachicagladiadora <200379@unsaac.edu.pe>
2024-05-31 21:46:31 -0500

    wip: search form

--- projects/05-react-search-movies/src/App.jsx
+++ projects/05-react-search-movies/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import './App.css'
 import { MoviesList } from './components/MoviesList'
 import { useMovies } from './hooks/useMovies'
@@ -7,10 +8,13 @@ import { useMovies } from './hooks/useMovies'
 function App() {
   const { movies: mappedMovies } = useMovies()
 
+  const inputRef = useRef()
 
   const onSubmit = (e) => {
     e.preventDefault()
+    const value = inputRef.current.value
     console.log('search')
+    console.log({ value })
   }
 
   return (
@@ -18,7 +22,7 @@ function App() {
       <header>
         <h1>Movies searcher</h1>
         <form onSubmit={(e) => onSubmit(e)}>
-          <input type="text" placeholder='The farewell concubine, Your name engraved' />
+          <input ref={inputRef} type="text" placeholder='The farewell concubine, Your name engraved' />
           <button>Search</button>
         </form>
       </header>
